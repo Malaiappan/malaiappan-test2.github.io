6e30dc6afd87cd7bae8ee4172078624db9494a6e
Malaiappan <malaiappan.kannan@gmail.com>
2016-10-19 22:25:29 +0200

    update with fetch

--- service-worker.js
+++ service-worker.js
@@ -17,3 +17,34 @@ self.addEventListener('install', function(e) {
     })
   );
 });
+
+
+//Adding `fetch` event listener
+self.addEventListener('fetch', function (event) {
+  console.info('Event: Fetch');
+
+  var request = event.request;
+
+  //Tell the browser to wait for newtwork request and respond with below
+  event.respondWith(
+    //If request is already in cache, return it
+    caches.match(request).then(function(response) {
+      if (response) {
+        return response;
+      }
+
+      //if request is not cached, add it to cache
+      return fetch(request).then(function(response) {
+        var responseToCache = response.clone();
+        caches.open(cacheName).then(
+          function(cache) {
+            cache.put(request, responseToCache).catch(function(err) {
+              console.warn(request.url + ': ' + err.message);
+            });
+          });
+
+        return response;
+      });
+    })
+  );
+});
