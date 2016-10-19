ed65c330d676679def69914b1a296856f7beb227
Malaiappan <malaiappan.kannan@gmail.com>
2016-10-19 22:50:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,3 +3,50 @@
              .register('./service-worker.js')
              .then(function() { console.log('Service Worker Registered'); });
   }
+
+
+getForecast = function(key, label) {
+    var statement = 'select * from weather.forecast where woeid=2459115';
+    var url = 'https://query.yahooapis.com/v1/public/yql?format=json&q=' +
+        statement;
+    // TODO add cache logic here
+    if ('caches' in window) {
+      /*
+       * Check if the service worker has already cached this city's weather
+       * data. If the service worker has the data, then display the cached
+       * data while the app fetches the latest data.
+       */
+      caches.match(url).then(function(response) {
+        if (response) {
+          response.json().then(function updateFromCache(json) {
+            var results = json.query.results;
+            results.key = key;
+            results.label = label;
+            results.created = json.query.created;
+           console.log(results);
+          //  app.updateForecastCard(results);
+          });
+        }
+      });
+    }
+    // Fetch the latest data.
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+      if (request.readyState === XMLHttpRequest.DONE) {
+        if (request.status === 200) {
+          var response = JSON.parse(request.response);
+          var results = response.query.results;
+          results.key = key;
+          results.label = label;
+          results.created = response.query.created;
+         console.log(results);
+         // app.updateForecastCard(results);
+        }
+      } else {
+        // Return the initial weather forecast since no data is available.
+      //  app.updateForecastCard(initialWeatherForecast);
+      }
+    };
+    request.open('GET', url);
+    request.send();
+  };
