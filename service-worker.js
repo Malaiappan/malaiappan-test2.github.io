422a2b2a6ca3995f34f9c19dff93ce0bfe06c3b9
Malaiappan <malaiappan.kannan@gmail.com>
2016-10-19 23:08:02 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -21,30 +21,34 @@ self.addEventListener('install', function(e) {
 
 //Adding `fetch` event listener
 self.addEventListener('fetch', function (event) {
-  console.info('Event: Fetch');
-
-  var request = event.request;
-
-  //Tell the browser to wait for newtwork request and respond with below
-  event.respondWith(
-    //If request is already in cache, return it
-    caches.match(request).then(function(response) {
-      if (response) {
-        return response;
-      }
-
-      //if request is not cached, add it to cache
-      return fetch(request).then(function(response) {
-        var responseToCache = response.clone();
-        caches.open(cacheName).then(
-          function(cache) {
-            cache.put(request, responseToCache).catch(function(err) {
-              console.warn(request.url + ': ' + err.message);
-            });
-          });
-
-        return response;
-      });
-    })
-  );
+  console.log('[Service Worker] Fetch', e.request.url);
+  var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+  if (e.request.url.indexOf(dataUrl) > -1) {
+    /*
+     * When the request URL contains dataUrl, the app is asking for fresh
+     * weather data. In this case, the service worker always goes to the
+     * network and then caches the response. This is called the "Cache then
+     * network" strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
+     */
+    e.respondWith(
+      caches.open(dataCacheName).then(function(cache) {
+        return fetch(e.request).then(function(response){
+          cache.put(e.request.url, response.clone());
+          return response;
+        });
+      })
+    );
+  } else {
+    /*
+     * The app is asking for app shell files. In this scenario the app uses the
+     * "Cache, falling back to the network" offline strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+     */
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  }
 });
