0ec8c8a74b7e7c056ffde9bd2ee520c7dd0139b3
Malaiappan <malaiappan.kannan@gmail.com>
2016-10-20 22:26:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,41 @@
          .register('./service-worker.js')
         .then(function(registration) { 
               console.log('Service Worker Registered');
-     
               return registration.pushManager.getSubscription()
-             .then(function(subscription) {
-                 if (subscription) {
-                   return subscription;
-                 }
-               return registration.pushManager.subscribe({ userVisibleOnly: true });
+              .then(function(subscription) {
+                // If a subscription was found, return it.
+                if (subscription) {
+                  return subscription;
+                }
+
+                // Otherwise, subscribe the user (userVisibleOnly allows to specify that we don't plan to
+                // send notifications that don't have a visible effect for the user).
+                return registration.pushManager.subscribe({ userVisibleOnly: true });
+              });
+            }).then(function(subscription) {
+              // Retrieve the user's public key.
+              var rawKey = subscription.getKey ? subscription.getKey('p256dh') : '';
+              key = rawKey ?
+                    btoa(String.fromCharCode.apply(null, new Uint8Array(rawKey))) :
+                    '';
+              var rawAuthSecret = subscription.getKey ? subscription.getKey('auth') : '';
+              authSecret = rawAuthSecret ?
+                           btoa(String.fromCharCode.apply(null, new Uint8Array(rawAuthSecret))) :
+                           '';
+
+              endpoint = subscription.endpoint;
+
+              // Send the subscription details to the server using the Fetch API.
+              fetch('./register', {
+                method: 'post',
+                headers: {
+                  'Content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                  endpoint: subscription.endpoint,
+                  key: key,
+                  authSecret: authSecret,
+                }),
               });
         });
   }
